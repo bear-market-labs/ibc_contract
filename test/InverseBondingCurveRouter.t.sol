4227f0a874665d54fd04d9e328d75aba7bb9b9fc
Sammy <changjiu.shao@gmail.com>
2023-11-01 19:52:27 +0800

    Fix TRST-L-4 Any funds sent directly to the Router contract would be lost

--- test/InverseBondingCurveRouter.t.sol
+++ test/InverseBondingCurveRouter.t.sol
@@ -49,6 +49,11 @@ contract InverseBondingCurveRouterTest is Test {
         _factoryContract = InverseBondingCurveFactory(_adminContract.factoryAddress());
     }
 
+    function testRevertIfDepositToRouter() public {
+        vm.expectRevert();
+        address(_router).call{value: 1 ether}('');
+    }
+
     function testInteractionWithETHPool() public {
         uint256 initialReserve = 2e18;
         uint256 buyReserve = 2e18;
