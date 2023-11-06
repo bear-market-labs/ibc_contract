ee3988a736f09d407000896d8f3b66e9a83cbdf1
ibjc <2389015+ibjc@users.noreply.github.com>
2023-11-06 04:45:37 +0000

    update deployment scripts to reflect all actors during deploy process

--- deployment/1_deployAdmin.sh
+++ deployment/1_deployAdmin.sh
@@ -1,11 +1,12 @@
 # deploy to tenderly mainnet fork network
 # forge script script/DeployAdmin.s.sol:DeployAdminScript --broadcast --verify --sender 0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266 --rpc-url https://rpc.tenderly.co/fork/cc2b5331-1bfa-4756-84ab-e2f2f63a91d5
 
+# run by deployer
 echo "Deploying Admin Contract:"
 mnemonic=$FOUNDRY_TEST_MNEMONIC
 rpc=$TENDERLY_RPC 
 export WETH_ADDRESS="0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
-export PROTOCOL_FEE_OWNER="0x70997970C51812dc3A010C7d01b50e0d17dc79C8"
+fee_owner=$PROTOCOL_FEE_OWNER #"0x70997970C51812dc3A010C7d01b50e0d17dc79C8"
 library_address=$IBC_CURVE_LIBRARY_ADDRESS #set to empty if we want redeploy
 if [ -z "$library_address" ]
 then
@@ -38,3 +39,16 @@ echo "Admin contract address: $IBC_ADMIN_CONTRACT_ADDRESS"
 echo "Admin contract owner address: $owner"
 echo "Admin contract fee owner address: $fee_owner"
 echo "Factory contract address: $IBC_FACTORY_CONTRACT_ADDRESS"
+
+echo "------------------------------------------"
+echo "Initiating ownership transfer to $OWNER"
+owner=$OWNER
+
+# two-stage transfer
+# 1. current owner designates pending owner
+cast send --mnemonic="$mnemonic" --rpc-url=$rpc $IBC_ADMIN_CONTRACT_ADDRESS "transferOwnership(address)" $owner
+
+# 2. pending owner claims ownership; can also do this thru a frontend + metamask + ledger
+#cast send --private-key="$OWNER_KEY" --rpc-url=$rpc $IBC_ADMIN_CONTRACT_ADDRESS "acceptOwnership()" 
+
+
--- deployment/2_CreateETHCurve.sh
+++ deployment/2_CreateETHCurve.sh
@@ -1,8 +1,8 @@
 mnemonic=$FOUNDRY_TEST_MNEMONIC
 rpc=$TENDERLY_RPC 
 empty_address=$(cast --address-zero)
-initial_lp_holder=0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
-initial_reserve=$(cast --to-wei 1 ether)
+initial_lp_holder=$INITIAL_LPER #0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
+initial_reserve=$(cast --to-wei 2 ether)
 echo "Create ibETH curve"
 cast send --mnemonic="$mnemonic" --rpc-url=$rpc $IBC_FACTORY_CONTRACT_ADDRESS --value $initial_reserve \
     "createCurve(uint256,address,address)" $initial_reserve $empty_address $initial_lp_holder
@@ -16,3 +16,5 @@ ibeth_address=$(cast --abi-decode "func()(address)" $call_result)
 export IBC_IBETH_CURVE_CONTRACT_ADDRESS=$curve_address
 echo "ibETH curve address: $curve_address"
 echo "ibETH token address: $ibeth_address"
+
+# run by deployer
